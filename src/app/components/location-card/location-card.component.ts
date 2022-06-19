8e23d1f235f80bff50b17492f86b9d85fb0043ce
Niklavs Romans <niklavsromans19@gmail.com>
2022-06-19 02:25:12 +0200

    locattion component

---
+++ src/app/components/location-card/location-card.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-location-card',
+  templateUrl: './location-card.component.html',
+  styleUrls: ['./location-card.component.scss'],
+})
+export class LocationCardComponent {
+  @Input() name = '';
+  @Input() dimension = '';
+  @Input() residents = 0;
+}
